b8b2de88454abb73c7d06375b1c44890a6060ea4
harshperson <harshperson@gmail.com>
2021-01-06 16:07:06 +0200

    12

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-async function start(){
-	try{
-		mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			database: 'store'
-		})
-		app.listen(PORT, () => {
-			console.log('Server has been started');
-		})
-	}catch{
-		console.log(e)
-	}
-}
-
-start()
-
-// connection.end(function(e) {
-// 	if (e) {
-// 	  console.log("Error: " + e.message);
+// async function start() {
+// 	try {
+// 		const connection = mysql.createPool({
+// 			host: 'localhost',
+// 			user: 'root',
+// 			database: 'store'
+// 		});
+// 		app.listen(PORT, () => {
+// 			console.log('Server has been started...');
+// 		});
+// 	} catch (e) {
+// 		console.log(e);
 // 	}
-// 	console.log("Connection closed");
-//   });
+// }
+
+// start();
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'store'
+});
+
+connection.connect();
+
+connection.query('SELECT * from auth');
+connection.end();
